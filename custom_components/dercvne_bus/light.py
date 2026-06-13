7e2d5065697537117ec9bef34e9dfc417a6307fe
Benjamin-LY777 <fzit@qq.com>
2026-06-13 10:05:50 +0800

    Delete custom_components/dercvne_bus directory

--- custom_components/dercvne_bus/light.py
+++
@@ -1,5 +0,0 @@
-"""Light platform for Dercvne Bus integration."""
-
-from .entities.light import async_setup_entry
-
-__all__ = ["async_setup_entry"]
--- custom_components/dercvne_bus/scene.py
+++
@@ -1,5 +0,0 @@
-"""Scene platform stub - delegates to entities module."""
-
-from .entities.scene import async_setup_entry
-
-__all__ = ["async_setup_entry"]
